3f43a524c7b7c8454e961438e79adce3020d2b9e
CHIULILE <chiutzuen.e.e@gmail.com>
2024-06-17 23:20:54 +0800

    Update script.js

--- E/script.js
+++ E/script.js
@@ -12,5 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.classList.remove('shake-effect');
         }, 5000); // 1000毫秒 = 1秒
         
+// 获取图片元素
+var image1 = document.getElementById('image1');
+
+// 点击图片切换函数
+function changeImage() {
+    // 检查当前显示的图片是 image1 还是 image2
+    if (image1.src.match("images/image1.jpg")) {
+        // 如果当前显示 image1，则切换到 image2
+        image1.src = "images/image2.jpg";
+    } else {
+        // 如果当前显示 image2，则切换回 image1
+        image1.src = "images/image1.jpg";
+    }
+}
+    
     });
 });
